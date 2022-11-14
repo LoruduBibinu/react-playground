f38ac4c452559dc3d200f8dfbb90282634166351
LoruduBibinu <77582493+LoruduBibinu@users.noreply.github.com>
2022-11-14 17:21:50 +0100

    pls marche

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
 const helloWorld = <h1>Hello world!</h1>;
+const lastName = "butcher" ;
+let firstNamelow ="billy";
+let firstName = firstNamelow.split("");
+for (let el in firstName){
+   firstName[0] = firstName[0].toUpperCase();
+}
+console.log(firstName);
+const prstvs = <section> <p> <span className="ln" >{lastName} </span> <span className="fn ">{firstName}</span></p></section>
+ReactDOM.render(
+    <div>
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+        {helloWorld}
+       {prstvs}
+    </div>,
+    document.querySelector('#app')
+);
