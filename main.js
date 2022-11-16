feae5a0ed879bf51def2ee0968466c95803cbf04
LoruduBibinu <77582493+LoruduBibinu@users.noreply.github.com>
2022-11-16 16:29:53 +0100

    ihgvjh

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
-function UserGreeting(props) {
-    return <h1>Bienvenue !</h1>;
-  }
-  
-  function GuestGreeting(props) {
-    return <h1>Veuillez vous connecter</h1>;
-  }
-  function Greeting(props) {
-      const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+function listing () {
 
-      const handleLogIn = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(true);
-      }
-
-      const handleLogOut = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(false);
-      }
-
-    return(
-        <React.Fragment>
-            {isLoggedIn ? 
-            <React.Fragment>
-                <UserGreeting />
-                <button onClick={handleLogOut}>Se déconnecter</button>
-            </React.Fragment> : 
-            <React.Fragment>
-                <GuestGreeting />
-                <button onClick={handleLogIn}>Se connecter</button>
-            </React.Fragment> }
-        </React.Fragment>
-    )
-  }
-  
+  const card = [];
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((list) => list.json())
+  .then((list) => 
+    for(let {name,email,company,website,phone} of list) {
+        let data = [name,email,company,website,phone]
+        card.push(data);
+        })
+    
+      
+ 
+  .catch((err) => {
+    console.log(err.message);
+  });
+return 
+     }
   ReactDOM.render(
-    <Greeting />,
+    <listing />,
     document.querySelector('#app')
   );
